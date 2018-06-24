6a5d3a0c1ad0423afe15b772a54bf4b377e5505d
Stephan Schrijver <stephan.schrijver@pon.com>
2018-06-24 11:51:56 +0200

    compile

--- inbox/src/compile.ts
+++ inbox/src/compile.ts
@@ -5,4 +5,4 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-console.log(solc.compile(source, 1));
+export const compile = solc.compile(source, 1).contracts[':Inbox'];
