d32e72bea853313841d5e674ae485e161f674810
Stephan Schrijver <stephanschrijver@gmail.com>
2018-06-30 10:38:33 +0200

    Test: Lottery allows one account to enter

--- lottery/spec/lottery.spec.ts
+++ lottery/spec/lottery.spec.ts
@@ -30,5 +30,19 @@ describe('Lottery', () => {
     it('deploys a contract', () => {
         expect(lottery.options.address).toBeTruthy();
     });
+
+    it('allows one account to enter', async ()=> {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        expect(accounts[0]).toEqual(players[0]);
+        expect(players.length).toEqual(1);
+    })
 });
 
